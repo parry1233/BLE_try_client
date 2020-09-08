be0fdd2781b110d316a0d36150ea5cc8f89a5cbf
parry1233 <ch.parry1233@gmail.com>
2020-09-08 23:32:55 +0800

    Initial commit

---
+++ BLE_try_client/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace BLE_try_client
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.btnSend = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textSend = new System.Windows.Forms.TextBox();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.btnConnect = new System.Windows.Forms.Button();
+			this.textStatus = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.panel3);
+			this.panel1.Controls.Add(this.panel2);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(358, 426);
+			this.panel1.TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel3.Controls.Add(this.btnSend);
+			this.panel3.Controls.Add(this.label3);
+			this.panel3.Controls.Add(this.textSend);
+			this.panel3.Location = new System.Drawing.Point(3, 132);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(352, 291);
+			this.panel3.TabIndex = 1;
+			// 
+			// btnSend
+			// 
+			this.btnSend.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+			this.btnSend.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+			this.btnSend.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnSend.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnSend.Location = new System.Drawing.Point(115, 250);
+			this.btnSend.Name = "btnSend";
+			this.btnSend.Size = new System.Drawing.Size(138, 36);
+			this.btnSend.TabIndex = 2;
+			this.btnSend.Text = "Send";
+			this.btnSend.UseVisualStyleBackColor = true;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(5, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(199, 25);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Message to Server";
+			// 
+			// textSend
+			// 
+			this.textSend.Location = new System.Drawing.Point(8, 28);
+			this.textSend.Multiline = true;
+			this.textSend.Name = "textSend";
+			this.textSend.Size = new System.Drawing.Size(328, 216);
+			this.textSend.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel2.Controls.Add(this.btnConnect);
+			this.panel2.Controls.Add(this.textStatus);
+			this.panel2.Controls.Add(this.label2);
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Location = new System.Drawing.Point(3, 3);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(352, 123);
+			this.panel2.TabIndex = 0;
+			// 
+			// btnConnect
+			// 
+			this.btnConnect.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+			this.btnConnect.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+			this.btnConnect.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnConnect.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnConnect.Location = new System.Drawing.Point(141, 79);
+			this.btnConnect.Name = "btnConnect";
+			this.btnConnect.Size = new System.Drawing.Size(138, 36);
+			this.btnConnect.TabIndex = 0;
+			this.btnConnect.Text = "Connect";
+			this.btnConnect.UseVisualStyleBackColor = true;
+			// 
+			// textStatus
+			// 
+			this.textStatus.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textStatus.Location = new System.Drawing.Point(88, 41);
+			this.textStatus.Name = "textStatus";
+			this.textStatus.Size = new System.Drawing.Size(250, 32);
+			this.textStatus.TabIndex = 0;
+			this.textStatus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(5, 41);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(77, 25);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Status";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Verdana", 14F);
+			this.label1.Location = new System.Drawing.Point(3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(202, 29);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Bluetooth Client";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(382, 450);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Client";
+			this.panel1.ResumeLayout(false);
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Button btnSend;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textSend;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Button btnConnect;
+		private System.Windows.Forms.TextBox textStatus;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ BLE_try_client/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DarrenLee.Bluetooth;
+
+namespace BLE_try_client
+{
+	public partial class Form1 : Form
+	{
+		Bluetooth_Client bleClient = new Bluetooth_Client("DELLXPC-PC");
+		public Form1()
+		{
+			InitializeComponent();
+			textStatus.Text = "------";
+		}
+
+		private void BtnConnect_Click(object sender, EventArgs e)
+		{
+			bleClient.Start();
+			textStatus.Text = "Trying......";
+			bleClient.IsConnected += bleClient_IsConnected;
+		}
+
+		private void bleClient_IsConnected(object sender, EventArgs e)
+		{
+			if (InvokeRequired)
+			{
+				this.Invoke(new Action(() =>
+				{
+					textStatus.Text = "Connected";
+				}));
+			}
+		}
+
+		private void BtnSend_Click(object sender, EventArgs e)
+		{
+			bleClient.SyncMessage(textSend.Text);
+			textSend.Text = null;
+		}
+	}
+}
